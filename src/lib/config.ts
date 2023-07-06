66a8e9b96e2656140a88c565f2cfa00854923266
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2023-07-06 10:08:54 +0530

    Initial Commit

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'Shakespeare';
+export const description = 'Sveltekit blog for poets';
+export const url = dev ? 'http://localhost:5173/' : 'https://shakespeare.vercel.app';
